0b0abfe8d2103913dadb154c03998ac307ae2658
Antonyeaster <>
2024-01-24 21:16:56 +0000

    Update Client origin for codeanywhere workspace

--- drf_api/settings.py
+++ drf_api/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 from pathlib import Path
 import os
 import dj_database_url
+import re
 
 if os.path.exists('env.py'):
     import env
@@ -63,7 +64,7 @@ DEBUG = True
 
 ALLOWED_HOSTS = [
     'localhost',
-    'drf-api-antony-c121376a2219.herokuapp.com'
+    os.environ.get('ALLOWED_HOST')
     ]
 
 
@@ -109,14 +110,12 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-if 'CLIENT_ORIGIN' in os.environ:
-    CORS_ALLOWED_ORIGINS = [
-         os.environ.get('CLIENT_ORIGIN')
-     ]
-else:
+if 'CLIENT_ORIGIN_DEV' in os.environ:
+    extracted_url = re.match(r'^([^.]+)', os.environ.get('CLIENT_ORIGIN_DEV', ''), re.IGNORECASE).group(0)
+
     CORS_ALLOWED_ORIGIN_REGEXES = [
-         r"^https://.*\.gitpod\.io$",
-     ]
+        rf"{extracted_url}.(eu|us)\d+\.codeanyapp\.com$",
+    ]
 
 ROOT_URLCONF = 'drf_api.urls'
 
